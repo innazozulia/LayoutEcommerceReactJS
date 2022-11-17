387c14d1b426f8b5d3fef633393fab1e25671234
innazozulia <innazozulia@outlook.com>
2022-11-17 19:49:04 +0200

    added main component for Home page, login and register page, slider, cart, single item

---
+++ client/app/src/App.js
@@ -0,0 +1,21 @@
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+
+function App() {
+  return (
+    <div className="app">
+      <Home />
+      {/* <ProductList /> */}
+      {/* <Product /> */}
+      {/* <Login /> */}
+      {/* <Register /> */}
+      {/* <Cart /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/app/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0608/1865/3402/products/alexandraemerald_400x.png?v=1655244249",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0608/1865/3402/products/nataliehotpink1_400x.jpg?v=1665071979",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://cdn.shopify.com/s/files/1/0608/1865/3402/products/NatalieGreenFront_400x.jpg?v=1665073893",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1607748862156-7c548e7e98f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1571721795195-a2ca2d3370a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHdvbWVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1607749091702-1bab1be215a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDh8fHdvbWVufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://assets.stickpng.com/thumbs/5baa31a3554c7f08176ebff2.png",
+  },
+  {
+    id: 2,
+    img: "https://assets.stickpng.com/thumbs/5c4dd5644f848b02250ff85f.png",
+  },
+  {
+    id: 3,
+    img: "https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43bedd.png",
+  },
+  {
+    id: 4,
+    img: "https://assets.stickpng.com/thumbs/5e90a021c7c8f9000434dd7a.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43bef1.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
